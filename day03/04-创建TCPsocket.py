d15d2400b96ddd2484dca36588c92362fd47451a
cess <270017772@qq.com>
2020-12-27 21:51:50 +0800

    提交

--- day03/04-创建TCPsocket.py
+++ day03/04-创建TCPsocket.py
@@ -1,7 +1,7 @@
 # 导入模块
 import socket
 
-# 创建套接字，使用IPv4 UDP方式
+# 创建套接字，使用IPv4 TCP方式
 # 参数一 协议类型：socket.AF_INET(IP4)   socket.AF_INET6(IP6)
 # 参数二 传输方式：socket.SOCK_DGRAM(UDP)     socket.SOCK_STREAM(TCP)
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
