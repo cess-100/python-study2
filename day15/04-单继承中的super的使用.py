45dcfd070d476ab6759ccf4781fea5d70a04fa8c
cess <270017772@qq.com>
2021-01-06 21:28:18 +0800

    提交

--- day15/04-单继承中的super的使用.py
+++ day15/04-单继承中的super的使用.py
@@ -7,15 +7,15 @@ class Parent(object):
 class Son1(Parent):
     def __init__(self, name, age):
         # super(Son1, self).__init__(name)
-        super().__init__(name)
         self.age = age
+        super().__init__(name)
         print('Son1的init结束被调用')
 
 
 class Grandson(Son1):
     def __init__(self, name, age, gender):
-        super().__init__(name, age)  # 单继承不能提供全部参数
         self.gender = "男"
+        super().__init__(name, age)  # 单继承不能提供全部参数
         print('Grandson的init结束被调用')
 
 
